b689fa895ff8bd01311ac037b08add56b44afef0
TheoGirardin <81571928+TheoGirardin@users.noreply.github.com@users.noreply.github.com>
2022-12-15 18:03:54 +0100

    json change and user test

--- user.sh
+++ user.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-
 echo  "####### USER #######"
+machine=$2
+user=$3
+json_file="/env/account.json"
 
 
-echo "$2@$3" #Nom de prompt a mettre 
-PS1="$user@$machine:~$" # set the prompt
+userCheck=$(jq -r '"$3"' /mnt/d/Stockage/Dev/LO14/lo14_project/env/account.json) #récupération du user dans le json
+echo "$userCheck"
+
+pwd=$(pwd)
+
+echo "  $pwd"
+
+if [ "$user" == "$userCheck" ]; then #vérification du user
+    echo "user found"
+else
+    echo "user not found"
+fi
+
+#machineCheck=$(jq -r $2 $json_file) #récupération de la machine dans le json
+
+#echo "$2@$3" #Nom de prompt a mettre 
+#PS1="$user@$machine:~$" # set the prompt
