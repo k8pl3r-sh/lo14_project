a632c27dad3a82eeab18b458148038e5e850a3ce
K8pl3r <k8pl3r@gmail.com>
2022-12-15 17:50:32 +0100

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -3,9 +3,10 @@
 ########################## FUNCTIONS #############################
 
 host () { # $1 = -a or -r and $2 is machine name
+	echo "DEBUG: host function"
 	if [ "$1" == "-a" ]; then
 		touch ./env/"$2"
-		# TODO: build json template with informations
+		# TODO: build json template with informations of admin (for first time)
 
 	elif [ "$1" == "-r" ]; then
 		rm ./env/"$2"
@@ -15,17 +16,36 @@ host () { # $1 = -a or -r and $2 is machine name
 }
 
 
-user () {
-	echo "user function"
+user () { # $1 = -ua/-ud or -ra/-rd and $2 is username, $3, boucle pls args hosts
+	# user: add/delete user, droits machines (for $#)
+	# -ua/ -ud <user> <*machines>
+
+	echo "DEBUG: user function"
+	if [ "$1" == "-ua" ]; then
+		# TODO: -user-add
+
+	elif [ "$1" == "-ur" ]; then
+		# TODO: -user-delete
+	fi
+
+	elif [ "$1" == "-ur" ]; then
+		# TODO: -right-add
+	fi
+
+	elif [ "$1" == "-ur" ]; then
+		# TODO: -right-delete
+	fi
 }
 
 
 wall () {
-	echo "wall function"
+	# syntax: wall message: for connected users
+	# wall -n message: for all users
+	echo "DEBUG: wall function"
 }
 
-afinger () {
-	echo "afinger function"
+afinger () { # $1 user
+	echo "DEBUG: afinger function"
 }
 
 
@@ -36,17 +56,21 @@ cmd="" # TODO: input command with options
 case $cmd in
 	host )
 		echo "host"
+		# function and arguments
 		;;
 
 	user )
 		echo "user"
+		# function and arguments
 		;;
 
 	wall )
 		echo "wall"
+		# function and arguments
 		;;
 
 	afinger )
 		echo "afinger"
+		# function and arguments
 		;;
 esac
