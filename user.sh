0de9dcb06f4f69e4047b5aa14cb049e6f47a52bc
TheoGirardin <81571928+TheoGirardin@users.noreply.github.com@users.noreply.github.com>
2022-12-16 13:00:52 +0100

    test connexion user

--- user.sh
+++ user.sh
@@ -2,20 +2,28 @@
 echo  "####### USER #######"
 machine=$2
 user=$3
-json_file="/env/account.json"
+json_file="env/account.json"
+userCheck=$(jq '"john"' $json_file)
+iMax=$(jq 'length' $json_file);
+
+
+for ((i=1; i<=$iMax; i++))
+do
+   userCheck=$(jq '[].name' $json_file)
+   echo "yo"
+
+   if [ "$userCheck" == "$user" ] 
+   then
+      echo "Bonjour Paul"
+   else
+      echo "Je ne connais pas ton nom"
+   fi
+done
 
 #REGEX jq pour accèder à l'array : .[].name
-userCheck=$(jq '"john"' env/account.json) #récupération du user dans le json
+ #récupération du user dans le json
 echo "$userCheck"
 echo "$user"
-
-if [ "$user" == "$userCheck" ]; then #vérification du user
-    echo "user $user found"
-else
-    echo "user not found"
-fi
-
-#machineCheck=$(jq -r $2 $json_file) #récupération de la machine dans le json
 
 #echo "$2@$3" #Nom de prompt a mettre 
 #PS1="$user@$machine:~$" # set the prompt
