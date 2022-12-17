845171c388f3bf4dd9eb332b35312d9e46e368b2
TheoGirardin <81571928+TheoGirardin@users.noreply.github.com@users.noreply.github.com>
2022-12-17 23:00:24 +0100

    suite

--- user.sh
+++ user.sh
@@ -12,9 +12,14 @@ for ((i=0; i<=$iMax; i++))
 do
    userQuoted=$(jq '.['$i'].name' $json_file)
    userCheck="${userQuoted:1:-1}" # remove quotes
-   if [ "$userCheck" = "$user" ] 
-   then
-      echo "Bonjour John"
+
+   permQuoted=$(jq '.['$i'].name' $json_file)
+   permCheck="${userQuoted:1:-1}" # remove quotes
+
+      #if [[ "$uservar"=="admin" && "$(echo "$passvar" | md5sum )"=="$passwd" ]]; then
+
+   if [ "$userCheck" = "$user" ]; then # TODO : rajouter une condition qui vérifie la permission de l'utilisateur pour la machine donné 
+      echo "Bonjour John" # TODO : 2e condition ou il doit rentrer le mot de passe + vérification du mot de passe en md5
    else
       continue
    fi
