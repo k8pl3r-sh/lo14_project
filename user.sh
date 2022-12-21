ee66c72119f0384dcdb35194dbc254a3acd67758
TheoGirardin <81571928+TheoGirardin@users.noreply.github.com@users.noreply.github.com>
2022-12-21 17:28:12 +0100

    changes

--- user.sh
+++ user.sh
@@ -4,29 +4,40 @@ echo  "####### USER #######"
 # Variables
 machine=$2
 user=$3
-json_file="env/account.json"
-iMax=$(jq 'length' $json_file);
 
 # Check if user is in json file
-for ((i=0; i<=$iMax; i++))
+for ((i=0; i<=$(jq 'length' env/account.json); i++))
 do
-   userQuoted=$(jq '.['$i'].name' $json_file)
-   userCheck="${userQuoted:1:-1}" # remove quotes
+   userQuoted=$(jq '.['$i'].name' env/account.json)
+   userCheck="${userQuoted:1:-1}"
 
-   permQuoted=$(jq '.['$i'].name' $json_file)
-   permCheck="${userQuoted:1:-1}" # remove quotes
+   if [ "$userCheck" = "$user" ]; then 
 
-      #if [[ "$uservar"=="admin" && "$(echo "$passvar" | md5sum )"=="$passwd" ]]; then
+      for ((j=0; j<=$(jq '.['$i'].permissions | length' env/account.json); j++))
+      do
+         permQuoted=$(jq '.['$i'].permissions['$j']' env/account.json)
+         permCheck="${permQuoted:1:-1}"
 
-   if [ "$userCheck" = "$user" ]; then # TODO : rajouter une condition qui vérifie la permission de l'utilisateur pour la machine donné 
-      echo "Bonjour John" # TODO : 2e condition ou il doit rentrer le mot de passe + vérification du mot de passe en md5
+         if [ "$permCheck" = "$machine" ]; then # TODO : 2e condition ou il doit rentrer le mot de passe + vérification du mot de passe en md5
+            echo "Bonjour $user" 
+            read -sp 'Password: ' passvar
+            passQuoted=$(jq '.['$i'].passwd' env/account.json)
+            passCheck="${passQuoted:1:-1}"
+               if [ "$(echo "$passvar" | md5sum )" == "$passCheck" ]; then
+                  echo "Mot de passe correct"
+                  exit 0
+               else
+                  echo "Mot de passe incorrect"
+                  exit 1
+               fi
+         fi    
+      done
    else
       continue
    fi
 done
 
-
-
+#if [[ "$uservar"=="admin" && "$(echo "$passvar" | md5sum )"=="$passwd" ]]; then
 
 #echo "$2@$3" #Nom de prompt a mettre 
 #PS1="$user@$machine:~$" # set the prompt
